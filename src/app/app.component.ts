4a9d2e2bf257c713d5a024d60579495eaed2238b
Gurveer-C1331 <45864502+Gurveer-C1331@users.noreply.github.com>
2023-04-27 13:45:47 -0400

    Blured search bar when "Enter/Go" is typed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-  title = 'website';
 
-  /** icons. */
-  public faClear = faXmark;
-  public faSearch = faMagnifyingGlass;
+    @ViewChild('searchBar') searchBar: ElementRef;
 
-  /** search string entered by the user. */
-  public searchString = { q : ''};
+    title = 'website';
 
-  constructor(public router: Router) { }
+    /** icons. */
+    public faClear = faXmark;
+    public faSearch = faMagnifyingGlass;
 
-  /**
-   * Route user to the Search results page and reset searchString.
-   */
-  public onSearch(): void {
+    /** search string entered by the user. */
+    public searchString = { q : ''};
 
-    if (this.searchString.q) {
-       this.router.navigate(
-        ['/search'],
-        { queryParams: this.searchString });
+    constructor(public router: Router) { }
 
-        this.searchString.q = '';
+    /**
+     * Route user to the Search results page and reset searchString.
+     */
+    public onSearch(): void {
+
+        if (this.searchString.q) {
+            this.router.navigate(
+            ['/search'],
+            { queryParams: this.searchString });
+
+            this.searchString.q = '';
+            this.searchBar.nativeElement.blur();
+        }
     }
-  }
 }
